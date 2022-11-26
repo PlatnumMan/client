6cae0bb4adb3eae8cfa9ef040181512a56863fd2
myles-ux <fistonrwanda@gmail.com>
2022-11-27 00:02:56 +0100

    stripe client setup

--- src/user/DashboardSeller.js
+++ src/user/DashboardSeller.js
@@ -1,8 +1,71 @@
 import { Link } from "react-router-dom";
 import DashboardNav from "../components/DashboardNav";
 import ConnectNav from "../components/ConnectNav";
+import { useSelector } from "react-redux";
+import toast from "react-hot-toast";
+import { HomeOutlined } from "@ant-design/icons";
+import { createConnectAccount } from "../actions/stripe";
+import { useState } from "react";
 
 const DashboardSeller = () => {
+  const { auth } = useSelector((state) => ({ ...state }));
+  const [loading, setLoading] = useState(false);
+
+  const handleClick = async () => {
+    setLoading(true);
+    try {
+      let res = await createConnectAccount(auth.token);
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+      toast.error("Something went wrong. Please try again");
+      setLoading(false);
+    }
+  };
+
+  const connected = () => (
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='col-md-10'>
+          <h2>Your Hotels</h2>
+        </div>
+        <div className='col-md-2'>
+          <Link to={`/hotels/new`} className='btn btn-primary'>
+            + Add New
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+  const notConnected = () => (
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='col-md-6 offset-md-3 text-center'>
+          <div className='pointer'>
+            <HomeOutlined className='h1' />
+            <h4>Set up payments to begin adding hotels.</h4>
+            <p className='lead'>
+              MERN and Stripe collaborate to transfer profits to your bank
+              account.
+            </p>
+            <button
+              disabled={loading}
+              onClick={handleClick}
+              className='btn btn-primary mb-3'
+            >
+              {loading ? "Processing..." : "Setup Payouts"}
+            </button>
+            <p className='text-muted'>
+              <small>
+                You will be directed to Stripe to finish the onboarding process.
+              </small>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
   return (
     <>
       <div className='container-fluid bg-secondary p-5'>
@@ -13,18 +76,12 @@ const DashboardSeller = () => {
         <DashboardNav />
       </div>
 
-      <div className='container-fluid'>
-        <div className='row'>
-          <div className='col-md-10'>
-            <h2>Your Bookings</h2>
-          </div>
-          <div className='col-md-2'>
-            <Link to={`/hotels/new`} className='btn btn-primary'>
-              + Add New
-            </Link>
-          </div>
-        </div>
-      </div>
+      {auth &&
+      auth.user &&
+      auth.user.stripe_seller &&
+      auth.user.stripe_seller.charges_enabled
+        ? connected()
+        : notConnected()}
     </>
   );
 };
